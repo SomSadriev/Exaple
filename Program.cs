435f5156039b97fbd47cc916a26b2175336b849b
SomSadriev <118583032+SomSadriev@users.noreply.github.com>
2022-12-11 15:26:27 +0300

    New version #3

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿
 Random random = new Random();
-int a = random.Next(0, 40);
+int a = random.Next(0, 100);
 
-if (a < 18)
+    if (a < 18)
     {
         Console.WriteLine($"Извените Вам нельзя пройти в клуб, посколкьу Вам нет 18лет, Вам - {a}");
     }
-    else
+    if (18 < a && a < 40)
     {
         Console.WriteLine($"Добро пожаловать в клуб, Вам - {a}");
     }
-
+    else
+    {
+        Console.WriteLine($"мдэээ, Вам {a} лет");
+    }
 
